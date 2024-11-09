e002a8c9a6d6d4211dd6a8abb286cd37ea423c2b
Bamo0507 <bryanalbertomo@gmail.com>
2024-11-08 20:51:27 -0600

    Progra defensiva MCD

--- euclides.py
+++ euclides.py
@@ -1,25 +1,24 @@
-def Algoritmo_euclides(a, b):
-   
-
-    # El bucle continúa hasta que el residuo (a % b) sea igual a 0.
-    while a % b != 0:
-        # Se calcula el residuo de a dividido por b.
-        r = a % b
+def mcd(a, b):
+    try:
+        #Verificación de que es un número entero positivo
+        if not isinstance(a, int) and not isinstance(b, int):
+            raise ValueError("Lamentablemente se envío en los parámetros algo que no era un entero.")
+        #El bucle continúa hasta que el residuo entre a y b sea 0
+        #Esto ya implicaría que hemos sacado el mcd, el cual sería el residuo de la iteración anterior
         
+        #Preguntar a Mario si así se maneja
+        if(a == 0 or b == 0):
+            return a if a != 0 else b
         
-        a = b
-        b = r
-
-    # Cuando b es igual a 0, el valor actual de a es el MCD, se retorna b que es el MCD.
-    return b
-
-
-# Se le solicita al usuario que ingrese dos números enteros positivos para a y b.
-a = int(input("Ingrese un entero positivo a: "))
-b = int(input("Ingrese un entero positivo b: "))
-
-
-RAlgoritmo = Algoritmo_euclides(a, b)
-
-# Imprime el resultado, que es el MCD de a y b.
-print(f"El mcd de {a} y {b} es {RAlgoritmo}")
+        while a % b != 0:
+            # Se calcula el residuo de a dividido por b.
+            r = a % b
+            a = b
+            b = r
+            
+        # Devolvemos 'b', que sería el MCD
+        return b
+    
+    except ValueError as e:
+        print(e)
+        return None
